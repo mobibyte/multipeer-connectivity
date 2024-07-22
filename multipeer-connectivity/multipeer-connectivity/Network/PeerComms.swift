fb9372bc727c565e3700f43ebb11e93a54c0f404
Diego V <diego@codewith.mobi>
2024-07-22 16:45:12 -0500

    docs: add comments

--- multipeer-connectivity/multipeer-connectivity/Network/PeerComms.swift
+++ multipeer-connectivity/multipeer-connectivity/Network/PeerComms.swift
@@ -5,6 +5,11 @@
 //  Created by Diego on 6/9/24.
 //
 
+/// Apple Developer Resources:
+// Article (the main source being followed): https://developer.apple.com/documentation/nearbyinteraction/discovering_peers_with_multipeer_connectivity
+// Sample code: https://developer.apple.com/documentation/nearbyinteraction/implementing_interactions_between_users_in_close_proximity
+// Framework overview: https://developer.apple.com/documentation/multipeerconnectivity
+
 import Foundation
 
 class PeerComms {
@@ -12,4 +17,6 @@ class PeerComms {
     func test() {
         print("hello")
     }
+    
+    /// Check for a Nearby Peer
 }
